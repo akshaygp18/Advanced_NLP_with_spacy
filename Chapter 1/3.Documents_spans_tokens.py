abb5be2ed7ccf8efe4287234e2e756e9b4ef210f
akshaygp18 <akshaygp41@gmail.com>
2023-09-13 18:38:40 +0530

    renamed the folder

--- Chapter 1/3.Documents_spans_tokens.py
+++
@@ -1,7 +0,0 @@
-## When you call nlp on a string, spaCy first tokenizes the text and creates a document object. 
-
-##Step 1:
-
-#Use spacy.blank to create the English nlp object.
-#Process the text and instantiate a Doc object in the variable doc.
-#Select the first token of the Doc and print its text.
