fc47f0774f44eb674a26fb58b1786721eebe2b25
akshaygp18 <akshaygp41@gmail.com>
2023-09-14 19:49:03 +0530

    trained_pipelines

--- Chapter_1/5.Trained_pipelines.py
+++ Chapter_1/5.Trained_pipelines.py
@@ -10,3 +10,33 @@ doc = nlp("she ate the pizza")
 for token in doc:
     # Print the text and the predicted part-of-speech tag
     print(token.text, token.pos_)
+
+
+#Predicting Syntactic Dependencies
+
+for token in doc:
+    print(token.text, token.pos_, token.dep_, token.head.text)
+
+
+#Predicting Named Entities
+
+# Process a text
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+
+# Iterate over the predicted entities
+for ent in doc.ents:
+    # Print the entity text and its label
+    print(ent.text, ent.label_)
+
+
+#spacy.explain method
+#Get quick definitions of the most common tags and labels.
+
+a = spacy.explain("GPE")
+print(a)
+
+b = spacy.explain("NNP")
+print(b)
+
+c = spacy.explain("dobj")
+print(c)
